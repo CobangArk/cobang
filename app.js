35651e8f8c0fec2d1c7970941b7f5c9e630a16e5
ukpono-j <ukponoakpan270gmail.com>
2021-09-11 10:38:44 +0100

    first commit

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 
   /* Open when someone clicks on the span element */
   function openNav() {
-    document.getElementById("myNav").style.width = "100%";
+    document.getElementById("myNav").style.width = "100px";
   }
   
   /* Close when someone clicks on the "x" symbol inside the overlay */
