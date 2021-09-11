1f6980d9b72c835722045c779eadb4085630e14f
ukpono-j <ukponoakpan270gmail.com>
2021-09-11 19:52:33 +0100

    debugging to find the source of errors

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 
   /* Open when someone clicks on the span element */
   function openNav() {
-    document.getElementById("myNav").style.width = "100px";
+    document.getElementById("myNav").style.width = "260px";
   }
   
   /* Close when someone clicks on the "x" symbol inside the overlay */
