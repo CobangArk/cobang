1df5f20f5881c43c093ce03c095ce9897031fc3d
ukpono-j <ukponoakpan270gmail.com>
2021-09-12 16:09:51 +0100

    making changes to the nature of the top and the side naivgation bar

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 
   /* Open when someone clicks on the span element */
   function openNav() {
-    document.getElementById("myNav").style.width = "260px";
+    document.getElementById("myNav").style.width = "240px";
   }
   
   /* Close when someone clicks on the "x" symbol inside the overlay */
@@ -9,10 +9,6 @@
     document.getElementById("myNav").style.width = "0%";
   }
   
-
-
-
-
 
 
 
